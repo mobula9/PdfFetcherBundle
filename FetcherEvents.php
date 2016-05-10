1c80f9605c3304961f60168360efee7ea0a954a7
Lucas CHERIFI <lucas@cherifi.info>
2016-05-10 23:28:56 +0200

    code style

--- FetcherEvents.php
+++ FetcherEvents.php
@@ -7,11 +7,7 @@ namespace Kasifi\PdfFetcherBundle;
 final class FetcherEvents
 {
     /**
-     * The CHANGE_PASSWORD_INITIALIZE event occurs when the change password process is initialized.
-     *
-     * This event allows you to modify the default values of the user before binding the form.
-     *
-     * @Event("FOS\UserBundle\Event\GetResponseUserEvent")
+     * @Event("Kasifi\PdfFetcherBundle\Event\CrawlFinishedEvent")
      */
     const CRAWL_FINISHED = 'kasifi_pdf_fetcher.events.crawl_finished';
 
